611e9d5bb9587284af1a1684b644cb6533d3f621
rasheeda <raskieda@gmail.com>
2024-03-25 14:52:52 +0200

    added guardians genres

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const songs = [
     { title: "Ain't No Mountain High Enough", artist: "Marvin Gaye & Tammi Terrell", genre: "R&B" },
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
+    { title: "Confessions", artist: "Usher", genre: "R&B" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    { title: "Break My Stride", artist: "Blue Lagoon", genre: "Dance" },
+    { title: "Save The World", artist: "Swedish House Mafia", genre: "Dance" },
+    { title: "Latch", artist: "Disclosure", genre: "Dance" }
     // Feel free to add even more songs
 ];
 
@@ -19,13 +23,16 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "R&B", 
+    "Rocket": "Dance",
+    "Groot": "Pop" + "Dance"
     // Add preferences for Drax, Rocket, and Groot
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
-    // Your code here
+    
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
