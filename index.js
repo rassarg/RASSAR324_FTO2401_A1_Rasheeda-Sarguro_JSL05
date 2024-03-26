a273d3f16035e564fb956d4ace7433de3c0d22f2
rasheeda <raskieda@gmail.com>
2024-03-26 16:27:43 +0200

    added songs and created playlists

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Array of song objects. Add at least 5 songs with title, artist, and genre properties.
-const songs = [
+const songList = [
     { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
     { title: "Moonage Daydream", artist: "David Bowie", genre: "Rock" },
     { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
@@ -12,9 +12,11 @@ const songs = [
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Confessions", artist: "Usher", genre: "R&B" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
-    { title: "Break My Stride", artist: "Blue Lagoon", genre: "Dance" },
-    { title: "Save The World", artist: "Swedish House Mafia", genre: "Dance" },
-    { title: "Latch", artist: "Disclosure", genre: "Dance" }
+    { title: "Break My Stride", artist: "Blue Lagoon", genre: "Pop" },
+    { title: "Wannabe", artist: "Spice Girls", genre: "Pop" },
+    { title: "Numb", artist: "Linkin Park", genre: "Rock" },
+    { title: "Revelry", artist: "Kings of Leon", genre: "Rock"},
+    { title: "Toxic", artist: "Britney Spears", genre: "Pop"}
     // Feel free to add even more songs
 ];
 
@@ -24,18 +26,25 @@ const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
     "Drax": "R&B", 
-    "Rocket": "Dance",
-    "Groot": "Pop" + "Dance"
+    "Rocket": "Rock",
+    "Groot": "Pop"
     // Add preferences for Drax, Rocket, and Groot
 };
 
-// Function to generate playlist based on preferred genre
-function generatePlaylist(guardians, songs) {
+
+// Function to generate playlist based on preferred genre 
+
+function generatePlaylist(guardians, songList) {
     // Use the map() function to create playlists for each Guardian
-    
-}
+    const starlordPlaylist = songList.map(songList => songList.genre === "Rock");
+    //console.log(starlordPlaylist.join(","));
+    const gamoraPlaylist = songList.map(songList => songList.genre === "Pop");
+    const draxPlaylist = songList.map(songList => songList.genre === "R&B");
+    const rocketPlaylist = songList.map(songList => songList.genre === "Rock");
+    const grootPlaylist = songList.map(songList => songList.genre === "Pop");
 
-// Call generatePlaylist and display the playlists for each Guardian
-generatePlaylist(guardians, songs);
-
+   
+}   
+//Call generatePlaylist and display the playlists for each Guardian
+generatePlaylist(guardians, songList);
 
